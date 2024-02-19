54008df68e5531611f492febf05d0db622f6d509
Jamsheed-Khan <jamshedkh365@gmail.com>
2024-02-19 18:06:13 +0500

    working

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import { Button, Checkbox, DatePicker, Form, Input } from 'antd';
+const onFinish = (values) => {
+    console.log('Success:', values);
+};
+const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+const App = () => (
+    <Form
+        name="basic"
+        labelCol={{
+            span: 8,
+        }}
+        wrapperCol={{
+            span: 16,
+        }}
+        style={{
+            maxWidth: 600,
+        }}
+        initialValues={{
+            remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+    >
+        <Form.Item
+            label="ITEM NAME"
+            name="itemName"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+
+        <Form.Item label="DATE"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}
+            name="date"
+        >
+            <DatePicker />
+        </Form.Item>
+
+
+        <Form.Item
+            label="AMOUNT"
+            name="amount"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+
+        <Form.Item
+            label="CATAGORY"
+            name="category"
+            rules={[
+                {
+                    required: true,
+                    message: 'Please input your username!',
+                },
+            ]}
+        >
+            <Input />
+        </Form.Item>
+
+
+
+        <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+                offset: 8,
+                span: 16,
+            }}
+        >
+            <Checkbox>Remember me</Checkbox>
+        </Form.Item>
+
+        <Form.Item
+            wrapperCol={{
+                offset: 8,
+                span: 16,
+            }}
+        >
+            <Button type="primary" htmlType="submit">
+                Submit
+            </Button>
+        </Form.Item>
+    </Form>
+);
+export default App;
